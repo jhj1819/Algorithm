d6e4c31dc7b455fdd1c57e33a89af21ab1793ec2
junghanjun <72265381+jhj1819@users.noreply.github.com>
2024-07-18 18:39:45 +0900

    [Gold III] Title: ACM Craft, Time: 288 ms, Memory: 132732 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,44 +1,40 @@
-import sys
+import io, os
+from collections import deque
 
-input = sys.stdin.readline
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
 #입력
 t = int(input())
 for _ in range(t):
     n, k = map(int, input().split())
 
-    times = list(map(int, input().split()))
+    time = [0] + list(map(int, input().split()))
+    graph = [[] for _ in range(n+1)]
 
-    next_list = [[] for _ in range(n+1)]
-    in_degree = [[0, 0] for _ in range(n+1)] ##(남은작업수, 현재까지 소요된 시간)
+    indegree = [0 for _ in range(n+1)]
+
+    dp = [0 for _ in range(n+1)]
     for _ in range(k):
-        a, b = map(int, input().split())
-        next_list[a].append(b)
-        in_degree[b][0] += 1
+        a,b = map(int, input().split())
+        graph[a].append(b)
+        indegree[b] += 1
 
-    goal = int(input())
-    is_goal = False
-    queue = []
-    for i in range(1, n+1): #초기에 진입차수 0인거 넣기
-        if in_degree[i][0] == 0:
-            if i == goal:
-                print(times[i-1])
-                is_goal = True
-                break
-            queue.append((i, 0))
+    w = int(input())
+    q = deque()
 
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+            dp[i] = time[i]
 
-
-    while not is_goal:
-        node, curr_time = queue.pop(0) #후에 시간초과난다면 deque로 변경..
-        curr_time = curr_time + times[node-1]  ##times 인덱스는 -1해줘야함
-        ##print(f"{node}일 끝냄 소요시간 {curr_time}")
-        for next_node in next_list[node]:
-            in_degree[next_node][0] -= 1
-            if in_degree[next_node][0] == 0:
-                queue.append((next_node, max(in_degree[next_node][1], curr_time))) ##진입차수가 0이 되는 시점이 최대시간이 아니므로 또 최댓값을 계산해줘야함
-                if next_node == goal:
-                    print(max(in_degree[next_node][1], curr_time) + times[goal-1])
-                    is_goal = True
+    while q:
+        pos = q.popleft()
+        for i in graph[pos]:
+            indegree[i] -= 1
+            dp[i] = max(dp[i], dp[pos] + time[i])
+            if indegree[i] == 0:
+                if i == w:
                     break
+                q.append(i)
 
-            in_degree[next_node][1] = max(in_degree[next_node][1], curr_time) ##times 인덱스는 -1해줘야함
+    print(dp[w])

5474bba7c8f844538f36f35fd3bd0de7833fd6e5
junghanjun <72265381+jhj1819@users.noreply.github.com>
2024-08-03 22:47:55 +0900

    [Gold IV] Title: 행성 연결, Time: 296 ms, Memory: 122148 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -4,10 +4,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-costs = []
-for _ in range(n):
-    costs.append(list(map(int, input().split())))
-    
+
+## 비용 인접행렬 배열 입력받음
+costs = [list(map(int, input().split())) for _ in range(n)]
+
 costs_heap = []
 for i in range(1, n):  # 일단 0번행성부터 출발
     costs_heap.append((costs[0][i], i))  # (i로 가는 비용, i)
@@ -15,6 +15,9 @@ heapq.heapify(costs_heap)
 
 visited = set()  #방문한 행성 번호 셋.
 visited.add(0)
+
+inf = float('INF')
+dist = [inf for _ in range(n)]
 
 count = 0
 result = 0
@@ -25,7 +28,8 @@ while count < n-1:
         count += 1
         result += next_edge[0]
         for i in range(n):  # 방문하지 않는것들을 힙에 넣는다
-            if i not in visited:
+            if i not in visited and costs[next_edge[1]][i] < dist[i]:
+                dist[i] = costs[next_edge[1]][i]
                 heapq.heappush(costs_heap, (costs[next_edge[1]][i], i))
 
 print(result)

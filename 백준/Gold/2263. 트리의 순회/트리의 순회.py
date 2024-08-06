04a4381ff7d64d2cb52910ff35ca38004cf05dcb
junghanjun <72265381+jhj1819@users.noreply.github.com>
2024-08-06 20:57:52 +0900

    [Gold I] Title: 트리의 순회, Time: 308 ms, Memory: 65036 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.py
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.py
@@ -5,12 +5,15 @@ sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 n = int(input())
-inorder = list(input().split())
-postorder = list(input().split())
+inorder = list(map(int, input().split()))
+postorder = list(map(int, input().split()))
 preorder = []
 
-# 인덱스 맵 생성
-inorder_index_map = {value: idx for idx, value in enumerate(inorder)}
+# 인덱스 배열
+idx_list = [0] * (n+1)
+for i in range(n):
+    idx_list[inorder[i]] = i
+
 
 def build_preorder(root_idx_postorder, left_idx_inorder, right_idx_inorder):
     global preorder
@@ -18,9 +21,8 @@ def build_preorder(root_idx_postorder, left_idx_inorder, right_idx_inorder):
     if left_idx_inorder > right_idx_inorder:
         return
 
-    # root_idx_inorder 구하기, find에서 시간소요가 클거같아.. 범위를 줄임 .. .그것도 안돼서 인덱스 맵..
-    # root_idx_inorder = inorder[left_idx_inorder:right_idx_inorder+1].index(postorder[root_idx_postorder]) + left_idx_inorder
-    root_idx_inorder = inorder_index_map[postorder[root_idx_postorder]]
+    # root_idx_inorder 구하기, find에서 시간소요가 클거같아.. 범위를 줄임 .. .그것도 안돼서 인덱스 맵or 인덱스배열..
+    root_idx_inorder = idx_list[postorder[root_idx_postorder]]
     preorder.append(postorder[root_idx_postorder])  # 이때 탐색하는 순서가 pre_order가 된다.
 
     # 왼쪽 서브트리의 루트는 현재 root에서 왼쪽 서브트리 크기만큼 이동
@@ -31,4 +33,4 @@ def build_preorder(root_idx_postorder, left_idx_inorder, right_idx_inorder):
 
 
 build_preorder(n - 1, 0, n - 1)
-print(' '.join(preorder))
+print(' '.join(map(str, preorder)))

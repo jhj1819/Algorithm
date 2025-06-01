021cab021b6b053f7f5d8cc3ed2958c9cf5c2a34
junghanjun <72265381+jhj1819@users.noreply.github.com>
2025-06-01 16:07:13 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -5,15 +5,18 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     cloth_dict = dict()
+    lst = []
 
     for i in range(n):
         name, category = input().split()
+        lst.append(category)
         if category not in cloth_dict:
             cloth_dict[category] = []
         cloth_dict[category].append(name)
 
     answer = 1
-    for cloth_dict_key in cloth_dict:
+    lst = list(set(lst))
+    for cloth_dict_key in lst:
         answer *= (len(cloth_dict[cloth_dict_key]) + 1)
     answer -= 1
 
